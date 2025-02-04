925b1bcc2172567c857fc2a8feccb1f0cfd5247e
AntonSimashkevich923985 <923985@pdsb.net>
2025-02-04 17:32:24 -0500

    Create Unit 1 Lesson 3.py

---
+++ Unit 1 Lesson 3.py
@@ -0,0 +1,26 @@
+print("There are ", 28, " people in this room.")  
+
+# Prints a sentence with the number 28.
+
+print("Your total money spent is: ", (2.75+9.32+11.91))  
+
+# Calculates and prints the total money spent using a simple sum.  
+
+print("There are %d people in this room." % 28)
+
+# Uses %d to insert the integer 28 into the string.  
+
+print("Pi is equal to %f" % 3.1416)
+
+# Prints the value of Pi with six decimal places.
+
+print("%s's final score is %d" % ("Joe", 215))  
+
+# Uses %s and %d to insert a string and an integer into the sentence.  
+
+print("Total money spent is: %.2f" % (2.75+9.32+11.91))  
+
+# Uses %.2f to round the total to two decimal places. 
+
+print("The area of a circle of radius %d is %f" % (4, 3.1416*4*4))  
+# Calculates the area of a circle and adds it into the string using %d and %f.  
