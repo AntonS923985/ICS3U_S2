155fc9a69ee1309ae236b28140bf5e9b7e4fa38a
AntonSimashkevich923985 <923985@pdsb.net>
2025-02-04 17:18:49 -0500

    Create Unit 1 Lesson 2.py

---
+++ Unit 1 Lesson 2.py
@@ -0,0 +1,36 @@
+#I predict the number 24 will be printed on the console.
+
+print(24)
+
+#My prediction was correct, the number 24 was printed.
+
+#I got the output I did because the print command is used to display text on a console, and when I ran it, the text in between the brackets was printed.
+
+#Modify 1
+
+amount = 24
+
+print("2 dozen is:" , amount)
+
+
+#Modify 2
+
+amount = 7*4
+
+print("Twenty eight is:" , amount)
+
+#Modify 3
+
+amount = 8
+amount = amount+1
+print("The amount is:" , amount)
+
+#This is happening because the + sign is telling the console to add the value of 1 to the current value of the amount.
+
+#Modify 4
+
+amount = 8
+amount = amount*2
+print("The amount is:" , amount)
+
+#This is happening because the * sign represents multiplication, therefore the console multiplies the amount variable by the number entered.
