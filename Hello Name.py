edebbf589e5522342c8d38891aada92edadfe8f0
AntonSimashkevich923985 <923985@pdsb.net>
2025-02-04 17:05:06 -0500

    Update Hello Name.py

--- Hello Name.py
+++ Hello Name.py
@@ -1,2 +1,11 @@
+#I predict the statement will print "Hello Leslie" to the console.
+
+name = "Leslie"
+print("Hello", name)
+
+#There was no comma because it was not in the quotations.
+
 name = "Anton"
-print("Hello," , name)
+print("Hello", name)
+
+#Variables act as containers for storing data and values.
