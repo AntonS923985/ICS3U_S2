3776eb7fdc8c1e2363af5cbb859893bcc7bf560d
AntonS923985 <923985@pdsb.net>
2025-02-06 23:14:24 -0500

    Create Unit 1 Lesson 6.py

---
+++ Unit 1 Lesson 6.py
@@ -0,0 +1,6 @@
+dob = int(input("Please input your year of birth:"))
+age = int(input("Please input your age:"))
+result = dob * 2 + 5
+result = result * 50 + age
+result = (result - 250) / 100
+print("Your birth year with your age as a decimal is:" , result)
