a4943ba13529a0714a687239d1539ee7ae24ca63
AntonS923985 <923985@pdsb.net>
2025-02-07 13:23:07 -0500

    Update Unit 1 Lesson 6.py

--- Unit 1 Lesson 6.py
+++ Unit 1 Lesson 6.py
@@ -3,4 +3,4 @@ age = int(input("Please input your age:"))
 result = dob * 2 + 5
 result = result * 50 + age
 result = (result - 250) / 100
-print("Your birth year with your age as a decimal is:" , result)
+print("Your birth year with your age added as a decimal is:" , result)
