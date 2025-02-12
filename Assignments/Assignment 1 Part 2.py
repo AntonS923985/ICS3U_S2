dee26c5fb824e8e8e8c49cf255fd5bcb86fbf521
AntonS923985 <923985@pdsb.net>
2025-02-12 13:08:24 -0500

    Create Assignment 1 Part 2.py

---
+++ Assignments/Assignment 1 Part 2.py
@@ -0,0 +1,8 @@
+import math
+print("This program will find the inner diagonal for any edge length provided!")
+length = int(input("Please input any non-zero number for the edge length of your cube:"))
+if(length == 0):
+    print("Edge length of zero will result in zero! Please try again.")
+else:
+    diagonal = length*math.sqrt(3)
+    print("The inner diagonal length for a cube with a side length of", length, "is", diagonal)
