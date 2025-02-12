e592e9469eeb005f0f87bb741173f2ea5de686a8
AntonS923985 <923985@pdsb.net>
2025-02-12 12:53:38 -0500

    Update and rename Assignment 1 Part 1 to Assignment 1 Part 1.py

---
+++ Assignments/Assignment 1 Part 1.py
@@ -0,0 +1,11 @@
+print("Welcome to the even or odd detector!")
+print("This program can determine if the product of two numbers is even or odd!")
+num1 = int(input("Please enter a number:"))
+num2 = int(input("Please enter another number:"))
+product = num1*num2
+product = int(product)
+rem = product % 2
+if(rem == 0):
+      print("The product of", num1, "*", num2, "is an even number.")
+else:
+    print("The produce of", num1, "*", num2, "is an odd number.")
