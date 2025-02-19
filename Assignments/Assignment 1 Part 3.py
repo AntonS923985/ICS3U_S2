28f35c2e4dbabdff6c874bbd78051e85ce8c1b2a
AntonS923985 <923985@pdsb.net>
2025-02-18 19:08:59 -0500

    Update Assignment 1 Part 3.py

--- Assignments/Assignment 1 Part 3.py
+++ Assignments/Assignment 1 Part 3.py
@@ -1,32 +1,27 @@
-import math
 change = int(input("Please input the amount of money in cents: "))
 
 print("Removing dollar value...")
 
-change = change % 100 # removes dollar value
-
-# variables must be defined outside "else" statements or an error will occur
+if change >= 100:
+    change = change % 100  # Removes dollar value if present
 
 qamt = change // 25
 remq = change % 25
 
-damt = remq // 10 
+damt = remq // 10
 remd = remq % 10
 
 namt = remd // 5
 remn = remd % 5
 
-pamt = remn // 1
+pamt = remn  # Directly the number of pennies left
 
-# rem for pennies is not needed
-
-if remq == 0:
-    print(change, "cents can be", qamt, "quarters.")
-if namt == 0:
-  print(change, "cents can be", qamt, "quarters,", namt, "nickels, and", pamt, "pennies.")
-elif remd == 0:
-    print(change, "cents can be", qamt, "quarters, and", damt, "dimes.")
-if namt == 0:
-  print(change, "cents can be", qamt, "quarters,", damt, "dimes, and", pamt, "pennies.")
-elif remn == 0:
-    print(change, "cents can be", qamt, "quarters,", damt, "dimes, and", namt, "nickels.")
+# Print the result based on nonzero coin values
+if qamt >= 0:
+  print(change, "cents can be", qamt, "quarters")
+if damt > 0:
+  print(damt, "dimes")
+if namt > 0:
+  print(namt, "nickels")
+if pamt > 0:
+  print("and", pamt, "pennies")
