d67d6771531e214e4ec072432932d8fc022179b6
AntonS923985 <923985@pdsb.net>
2025-02-21 13:59:55 -0500

    Update Assignment 1 Part 3.py

--- Assignments/Assignment 1 Part 3.py
+++ Assignments/Assignment 1 Part 3.py
@@ -2,25 +2,28 @@ change = int(input("Please input the amount of money in cents: ")) # Asks the us
 
 print("Checking for value above one dollar...") # Informs the user that dollar amount is being removed if they inputted above a dollar
 
-if change >= 100:
+if change >= 100: # Preforms a check for value above 100
     change = change % 100  # Removes dollar value if present
 
-qamt = change // 25
-remq = change % 25
+# Assigns variables to the coin amounts and remainders
 
-damt = remq // 10
-remd = remq % 10
+qamt = change // 25 # Quarter amount
+remq = change % 25 # Quarter remainder
 
-namt = remd // 5
-remn = remd % 5
+damt = remq // 10 # Dime amount
+remd = remq % 10 # Dime remainder
+
+namt = remd // 5 # Nickel amount
+remn = remd % 5 # Nickel remainder
 
 pamt = remn  # Directly the number of pennies left
 
-# Prints the results, not including coins that are not included in the final result
+# The following code prints the results, not including coins that are equal to zero in the final result
+
 if qamt == 0:
   print("You have one dollar.") # If 100 is entered, returns with this message instead of zero quarters
 else:
-  print(change, "cents can be:")
+  print(change, "cents can be:") 
 if qamt > 0:
   print(qamt, "quarters")
 if damt > 0:
