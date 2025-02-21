9f77e7576963b013cd0df24ee1fbae9f7eba70d4
AntonS923985 <923985@pdsb.net>
2025-02-21 13:58:53 -0500

    Update Assignment 1 Part 2.py

--- Assignments/Assignment 1 Part 2.py
+++ Assignments/Assignment 1 Part 2.py
@@ -3,6 +3,7 @@ import math # Imports python math functions
 print("This program will find the inner diagonal for any edge length provided!") # Informs the user of what the program does
 
 length = int(input("Please input any non-zero number for the edge length of your cube:")) # Asks the user to provide a number for the side length
+
 if(length == 0): # Checks if the previous input is zero
     print("Edge length of zero will result in zero! Please try again.") # included error message in case zero is inputted
 else: 
