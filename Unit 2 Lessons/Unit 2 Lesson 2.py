4bfcaff67fbb26f084875257bf77c01c2bbf525a
AntonS923985 <923985@pdsb.net>
2025-03-04 12:01:15 -0500

    Update Unit 2 Lesson 2.py

--- Unit 2 Lessons/Unit 2 Lesson 2.py
+++ Unit 2 Lessons/Unit 2 Lesson 2.py
@@ -26,7 +26,7 @@ trinum = 1 # Actual triangular number. (1, 3, 6, 10, 15...)
 addnum = 2 # Number to be added to the triangular number each time
 toaddnum = 1 # Value to be added to the value of the number being added
 while trinumcount <= 6: # Which stage of the triangular number the program will count to (If 3 is put instead of 6, for example, the output will be 1, 3, 6.)
-  if trinumcount == 1:
+  if trinumcount == 1: # Since the first, second, and third
     print("The %dst triangluar number is %d" %  (trinumcount, trinum))
     trinum += addnum
     addnum += toaddnum
