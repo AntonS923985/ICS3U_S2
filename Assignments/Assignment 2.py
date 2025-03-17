387bba40cd86687642963e33f60818032638fd04
AntonS923985 <923985@pdsb.net>
2025-03-17 14:03:05 -0400

    Create Assignment 2.py

---
+++ Assignments/Assignment 2.py
@@ -0,0 +1,19 @@
+"""
+Author : Joe Student
+   Revison date : 10 July 2023
+   Program : Bubble Sort Assignment
+   Description : a program to sort a list of 100 people
+     by their names and heights 
+   VARIABLE DICTIONARY :
+     Fullname (String) = person's name
+     height (double) = person's height
+     Lastname (String) = string object for entering the last-names
+     Firstname (String) = string object for entering the *first-names
+     B (String) = a variable for ignoring the first line in the file
+     sum (double) = variable for the total sum of the heights
+     avg (double) = variable for the average of all heights
+     count (int) = the number of people averaged
+"""
+
+import random
+num = random.randrange(1,100,1)
