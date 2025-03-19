e655cf8f70ecc8c710651415cc66ca321b5442d2
AntonS923985 <923985@pdsb.net>
2025-03-19 13:21:30 -0400

    Update Assignment 2.py

--- Assignments/Assignment 2.py
+++ Assignments/Assignment 2.py
@@ -10,6 +10,18 @@ Author : Anton Simashkevich
      guess (int) = a guess inputted by the user
 """
 
+"""
+Author : Anton Simashkevich
+   Revison date : 18 March 2025
+   Program : A Number Guessing Game Assignment
+   Description : A number guessing game with a range of numbers from 1 to 100.
+   VARIABLE DICTIONARY :
+     num (int) = random number chosen by the program
+     guessnum (String) = amount of guesses given to the user
+     correct (Boolean) = a check if the users guess is correct or not.
+     guess (int) = a guess inputted by the user
+"""
+
 import random # allows for random number generation
 num = random.randrange(1, 100, 1) # the range in which numbers can be generated, 1 to 100 in this case
 guessnum = 6 # sets the number of guesses to 6
@@ -22,9 +34,9 @@ while not correct: # loops while a correct answer has not been inputted
     print("You got it! Nice job.") # informs the user that they guessed correctly and stops further guessing
   if(guess != num): # checks if the guess is not equal to the generated number
     guessnum -= 1 # subtracts 1 guess from the total amount of guesses and assigns the new value to guessnum
-  if(guess > num): # checks if the guess is greater than the generated number
+  if(guess > num and guessnum > 1): # checks if the guess is greater than the generated number
     print("Try guessing lower. You have %d guesses left" % guessnum) # if previous check is true, tells the user to guess a lower number and lets them know how many guesses remain
-  if(guess < num): # checks if the guess is less than the generated number
+  if(guess < num and guessnum > 1): # checks if the guess is less than the generated number
     print("Try guessing higher. You have %d guesses left" % guessnum) # if previous check is true, tells the user to guess a higher number and lets them know how many guesses remain
   if(guessnum == 1 and guess > num): # included so the print statement wouldn't say "you have one guesses remaining", instead if "you have one guess remaining", for proper grammar
     print("Try guessing lower. You have %d guess left." % guessnum) # informs the user to guess lower in the case of the number of guesses being equal to 1
