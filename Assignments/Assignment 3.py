edc1893036d09b8f9dc55467b8e79f79be3fad2e
AntonS923985 <923985@pdsb.net>
2025-04-08 12:58:57 -0400

    Update Assignment 3.py

--- Assignments/Assignment 3.py
+++ Assignments/Assignment 3.py
@@ -2,8 +2,5 @@ list = ["civic", "level", "kayak", "madam", "tacocat", "racecar", "student", "ro
 for c in range(len(list)):
     word = list[c]
     wlen = len(word) - 1
-    print(word[0], word[wlen])
-for d in range(len(list)):
-    word = list[d]
-    wlen = len(word) - 2
-    print(word[1], word[wlen])
+    for d in range(len(word)):
+      print(word[d], word[wlen-d])
