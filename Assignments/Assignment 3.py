f3a624b903568c03a547183467e774c299419964
AntonS923985 <923985@pdsb.net>
2025-04-08 12:32:32 -0400

    Create Assignment 3.py

---
+++ Assignments/Assignment 3.py
@@ -0,0 +1,5 @@
+list = ["civic", "level", "kayak", "madam", "tacocat", "racecar", "student", "rotor", "problem", "brick"]
+for c in range(len(list)):
+    word = list[c]
+    wlen = len(word) - 1
+    print(word[0], word[wlen], word)
