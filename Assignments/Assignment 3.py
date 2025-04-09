4da5b51b5925d1663182418e59e0ab8bcd95e612
AntonS923985 <923985@pdsb.net>
2025-04-08 21:21:39 -0400

    Update Assignment 3.py

--- Assignments/Assignment 3.py
+++ Assignments/Assignment 3.py
@@ -1,3 +1,17 @@
+"""
+Author           : Anton Simashkevich
+Revision date    : April 8th 2025
+Program          : Palindrome Checker
+Description      : A program that checks whether words in a list are palindromes or not by comparing their letters.
+
+VARIABLE DICTIONARY :
+  list           (List of Strings)   = A list containing the words to be checked for palindrome status.
+  word           (String)            = The current word being checked from the list.
+  wlen           (int)               = The index of the last character in the current word (length of the word - 1).
+  is_palindrome  (Boolean)           = A flag that determines whether the current word is a palindrome.
+  d              (int)               = The index used to compare characters from the front and back of the word.
+"""
+
 list = ["civic", "level", "kayak", "madam", "tacocat", "racecar", "student", "rotor", "problem", "brick"] # Define a list of 10 words, some palindromes and some not
 
 print("Palindrome program!") # Print the starting message
