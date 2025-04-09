dab4228fb1433ecdc41f168f3b68c3bb13f3acb6
AntonS923985 <923985@pdsb.net>
2025-04-08 21:16:36 -0400

    Update Assignment 3.py

--- Assignments/Assignment 3.py
+++ Assignments/Assignment 3.py
@@ -1,6 +1,21 @@
-list = ["civic", "level", "kayak", "madam", "tacocat", "racecar", "student", "rotor", "problem", "brick"]
-for c in range(len(list)):
-    word = list[c]
-    wlen = len(word) - 1
-    for d in range(len(word)):
-      print(word[d], word[wlen-d])
+list = ["civic", "level", "kayak", "madam", "tacocat", "racecar", "student", "rotor", "problem", "brick"] # Define a list of 10 words, some palindromes and some not
+
+print("Palindrome program!") # Print the starting message
+
+for c in range(len(list)): # Loop through each word in the list using its index
+    word = list[c] # Get the current word from the list
+    wlen = len(word) - 1 # Store the index of the last character in the word
+    is_palindrome = True # Assume the word is a palindrome to start
+    d = 0 # Start comparing from the first character
+
+    while d < len(word) // 2:  # Loop while d is less than half the length of the word (only need to check halfway)
+        if word[d] != word[wlen - d]: # If the character from the front doesn't match the corresponding back character
+            is_palindrome = False # It's not a palindrome
+            d = len(word) # Force the loop to exit by pushing d past the limit
+        else:
+            d += 1 # Move to the next character toward the center
+            
+    if is_palindrome:
+        print("%s is a palindrome" % word) # If no mismatches were found
+    else:
+        print("%s is not a palindrome" % word) # If a mismatch was found
