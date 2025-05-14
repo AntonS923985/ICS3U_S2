cd1eb3bda4c6f06f69ce7e61ac000dd9f09b4b1e
AntonS923985 <923985@pdsb.net>
2025-05-14 12:26:43 -0400

    Update Unit 2 Lesson 3.py

--- Unit 2 Lessons/Unit 2 Lesson 3.py
+++ Unit 2 Lessons/Unit 2 Lesson 3.py
@@ -12,9 +12,7 @@ print("%d is equal to %d" % (num, fact))
 
 n = int(input("Please enter a number: "))
 count = n
-
 print(n, "factorial is equal to ", end="")
-
 while count > 0:
   if count == 1:
     print("1")
@@ -27,10 +25,7 @@ while count > 0:
 n = int(input("Please enter a number: "))
 count = n
 fac = 1
-
 while count > 0:
-  
     fac *= count
     count -= 1
-    
 print(n, "factorial is equal to", fac)
