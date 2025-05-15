20b1ae5b07188be71ae9420d1c4ed676d148d9ee
AntonS923985 <923985@pdsb.net>
2025-05-15 13:39:33 -0400

    Create Unit 3 Lesson 1.py

---
+++ Unit 3 Lessons/Unit 3 Lesson 1.py
@@ -0,0 +1,11 @@
+# Place your code here!
+
+# Modify 1
+
+progname = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in."
+spaces = 0
+for i in progname:
+  if i == " ":
+    spaces +=  1
+    
+print("There are %d spaces in the text." % spaces)
