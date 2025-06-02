3b89f98377bf41bb77113c9563ad6ebd509d489c
AntonS923985 <923985@pdsb.net>
2025-06-02 13:07:40 -0400

    Update Assignment 5 FINAL ASSIGNMENT.py

--- Assignments/Assignment 5 FINAL ASSIGNMENT.py
+++ Assignments/Assignment 5 FINAL ASSIGNMENT.py
@@ -1,1 +1,33 @@
+"""
+Author : Anton Simashkevich
+Student number: 923985
+Course code: ICS3U
+Revision date : June 2nd, 2025
+Program : Credit Card Report
+Description : A program that accesses a credit card report file, checks which cards are expired, and returns
+              them in the form of a txt file.
+
+VARIABLE DICTIONARY :
+  GivenName (list) = Stores the first names of customers
+  Surname (list) = Stores the last names of customers
+  CCType (list) = Stores the credit card types (Visa, Mastercard, etc.)
+  CCNumber (list) = Stores credit card numbers
+  Exp-Mo (list) = Stores the expiry months of the credit cards in abbreviated months (Jan, Feb, etc.)
+  EXP-Yr (list) = Stores the expiry years of the credit card numbers
+  
+
+
+
+
+
+
+  
+"""
+
+GivenName = []
+Surname = []
+CCType = []
+CCNumber = []
+Exp-Mo = []
+Exp-Yr = []
 
